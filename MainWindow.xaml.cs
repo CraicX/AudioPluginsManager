ae333a20a8f58fe607fbd3a616b85f3cb9d3b326
D H <46064123+CraicX@users.noreply.github.com>
2023-12-04 09:05:34 -0800

    Layout

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,10 +21,57 @@ namespace AudioPluginsManager;
 /// </summary>
 public partial class MainWindow : AdonisWindow
 {
+    public bool IsVendorsVisible{ get; set; }     = true;
+    public bool IsFXVisible{ get; set; }          = true;
+    public bool IsInstrumentsVisible{ get; set; } = true;
+
     public MainWindow()
     {
         InitializeComponent();
 
         Config.Init(this);
     }
+
+    private void TglVendors_Click(object sender, RoutedEventArgs e)
+    {
+        IsVendorsVisible = !IsVendorsVisible;
+        UpdateToggleButtons();
+    }
+
+    private void TglFX_Click(object sender, RoutedEventArgs e)
+    {
+        IsFXVisible = !IsFXVisible;
+        UpdateToggleButtons();
+    }
+
+    private void TglInstruments_Click(object sender, RoutedEventArgs e)
+    {
+        IsInstrumentsVisible = !IsInstrumentsVisible;
+        UpdateToggleButtons();
+    }
+
+    private void UpdateToggleButtons()
+    {
+        BitmapImage bi = new();
+        bi.BeginInit();
+        bi.UriSource = new Uri("Resources/img/" + (IsVendorsVisible ? "t-vendor.png" : "t-vendor2.png"), UriKind.Relative);
+        bi.EndInit();
+        ImgVendors.Source     = bi;
+
+        bi = new();
+        bi.BeginInit();
+        bi.UriSource = new Uri("Resources/img/" + (IsFXVisible ? "t-fx.png" : "t-fx2.png"), UriKind.Relative);
+        bi.EndInit();
+        ImgFX.Source = bi;
+
+        bi = new();
+        bi.BeginInit();
+        bi.UriSource = new Uri("Resources/img/" + (IsInstrumentsVisible ? "t-instruments.png" : "t-instruments2.png"), UriKind.Relative);
+        bi.EndInit();
+        ImgInstruments.Source = bi;
+
+        //ImgFX.Source          = new BitmapImage(new Uri("Resources/img/" + (IsFXVisible ? "t-fx.png" : "t-fx2.png")));
+        //ImgInstruments.Source = new BitmapImage(new Uri("Resources/img/" + (IsInstrumentsVisible ? "t-instruments.png" : "t-instruments2.png")));
+    }
+
 }
